239d76c5a245c8b2eaaa0b8bdc1b32f9fd5df02c
Rajni Gupta <rajnigupta2901@gmail.com>
2021-12-23 22:57:06 +0530

    added

--- AgressiveCow.cpp
+++ AgressiveCow.cpp
@@ -1,3 +1,4 @@
+//Aggressive Cow : Advanced BS Problem
 #include <iostream>
 using namespace std;
 #include <utility>
@@ -22,6 +23,7 @@ bool isPossible(vector<int> &stalls,int mid,int k){
     }
     return false;
 }
+
 
 int main()
 {
